63e807fcffe46a848a51cbc320a61fdaf92c0ba5
dario <dariomacielr@gmail.com>
2025-02-17 19:41:05 -0300

    cambios

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let malbon = {
     img: "img/malbon.jpg",
     descripcion: "Una planta elegante y versátil, perfecta para dar un toque fresco y natural a cualquier espacio interior. Sus hojas vibrantes y su fácil cuidado la convierten en una favorita para principiantes y expertos por igual."
 };
-
 let cactus = {
     nombre: "Cactus",
     tipo: "exterior",
     riego: "cada 7 días",
     luz: "luz directa",
     img: "img/cactus.jpg",
-    descripcion: "Ideal para aquellos que buscan una planta resistente y de bajo mantenimiento. Su aspecto único y su capacidad para sobrevivir en condiciones áridas lo hacen perfecto para decorar espacios secos y soleados."};
+    descripcion: "Ideal para aquellos que buscan una planta resistente y de bajo mantenimiento. Su aspecto único y su capacidad para sobrevivir en condiciones áridas lo hacen perfecto para decorar espacios secos y soleados."
+};
  let suculenta = {
     nombre: "Suculenta",
     tipo: "interior",
@@ -24,7 +24,6 @@ let cactus = {
     img: "img/suculenta.jpg",
     descripcion: "Con sus formas geométricas y colores variados, las suculentas son pequeñas obras de arte naturales. Son excelentes para crear arreglos decorativos y requieren muy poca atención."
 };
-
 let ficus = {
     nombre: "Ficus",
     tipo: "interior",
@@ -34,13 +33,16 @@ let ficus = {
     descripcion: "Una planta clásica y atemporal, conocida por su capacidad para purificar el aire. Su follaje denso y brillante añade un toque de elegancia y frescura a cualquier ambiente."
 };
 
-plantas.push(malbon);
+plantas.push(malbon,cactus,suculenta,ficus);
 
 function agregarPlanta(plantas) {
     plantas.forEach(planta => {
         let articulo = document.createElement("article");
         let img = document.createElement("img");
         img.src = `img/${planta.nombre.toLowerCase()}.jpg`;
+        img.alt = planta.nombre;
+        imageContainer.appendChild(img);
+        articulo.appendChild(imageContainer);
         let h3 = document.createElement("h3");
         h3.textContent = planta.nombre;
         let p = document.createElement("p");
